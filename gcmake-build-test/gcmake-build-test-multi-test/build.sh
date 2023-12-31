ef8835134feae4becb7870214c52af5947518f59
cogo152 <gokay.comert@icloud.com>
2023-12-31 16:54:02 +0100

    update properties files

--- gcmake-build-test/gcmake-build-test-multi-test/build.sh
+++ gcmake-build-test/gcmake-build-test-multi-test/build.sh
@@ -3,12 +3,10 @@
 rm -rf build
 
 cmake   --no-warn-unused-cli                        \
-        -DCMAKE_BUILD_TYPE:STRING=Release           \
         -S.                                         \
         -B build                                    \
         -G "Unix Makefiles"                         \
 
 cmake   --build build                               \
-        --config Release                            \
         --target all test                           \
         -j 14 --                                    \
